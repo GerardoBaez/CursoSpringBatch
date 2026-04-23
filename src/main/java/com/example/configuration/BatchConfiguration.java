08379eecba1bac0822f6a753acef0ff59b935889
Gerardo Baez Molina <gbaezmolina@gmail.com>
2026-04-23 10:07:52 -0600

    Se sube codigo inicial

--- src/main/java/com/example/configuration/BatchConfiguration.java
+++ src/main/java/com/example/configuration/BatchConfiguration.java
@@ -7,13 +7,18 @@ import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
 
 import com.example.decider.MyJobExecutionDecider;
 import com.example.listener.MyStepExecutionListener;
@@ -25,11 +30,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class BatchConfiguration {
 
-	@Autowired
-	public JobBuilderFactory jobBiulderFactory;
-	
-	@Autowired
-	public StepBuilderFactory stepBiulderFactory;
 	
 	@Bean
 	public JobExecutionDecider decider() {
@@ -42,19 +42,22 @@ public class BatchConfiguration {
 	}
 	
 	@Bean 
-	public Step step1() {
-		return this.stepBiulderFactory.get("step1").tasklet(new Tasklet(){
+	public Step step1(JobRepository jobrep, PlatformTransactionManager tx) {
+		return new StepBuilder("step1", jobrep).tasklet(new Tasklet(){
 			@Override
 			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 				log.info("step1 executed!!");
 				return RepeatStatus.FINISHED;
 			}
-		}).build();
+		},tx).build();
 	}
 	
+	
+	
+	
 	@Bean 
-	public Step step2() {
-		return this.stepBiulderFactory.get("step2").tasklet(new Tasklet(){
+	public Step step2(JobRepository jobrep, PlatformTransactionManager tx) {
+		return new StepBuilder("step2",jobrep).tasklet(new Tasklet(){
 			@Override
 			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 				boolean fail= false;
@@ -64,41 +67,41 @@ public class BatchConfiguration {
 				log.info("step2 executed!!");
 				return RepeatStatus.FINISHED;
 			}
-		}).build();
+		},tx).build();
 	}
 	
 	@Bean 
-	public Step step3() {
-		return this.stepBiulderFactory.get("step3").tasklet(new Tasklet(){
+	public Step step3(JobRepository jobrep, PlatformTransactionManager tx) {
+		return new StepBuilder("step3",jobrep).tasklet(new Tasklet(){
 			@Override
 			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 				log.info("step3 executed!!");
 				return RepeatStatus.FINISHED;
 			}
-		}).build();
+		}, tx).build();
 	}
 	
 	@Bean 
-	public Step step4() {
-		return this.stepBiulderFactory.get("step4").tasklet(new Tasklet(){
+	public Step step4(JobRepository jobrep, PlatformTransactionManager tx) {
+		return new StepBuilder("step4",jobrep).tasklet(new Tasklet(){
 			@Override
 			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 				log.info("step4 executed!!");
 				return RepeatStatus.FINISHED;
 			}
-		}).build();
+		}, tx).build();
 	}
 	
 	@Bean 
-	public Job firstJob() {
+	public Job firstJob(JobRepository jobrep,Step step1, Step step2, Step step3, Step step4 ) {
 		
 		
 		
-		return this.jobBiulderFactory.get("job1")
-		.start(step1()).on("COMPLETED").to(decider())
-										.on("TEST_STATUS").to(step2())
-										.from(decider())
-										 .on("*").to(step3())
+		return new JobBuilder("job1",jobrep)
+		.start(step1).on("COMPLETED").to(decider())
+										.on("TEST_STATUS").to(step2)
+					  .from(decider())
+										 .on("*").to(step3)
 									 
 										 
 										 
